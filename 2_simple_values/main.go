73bda48bf126ab5b65654005da8e2001fdb94d75
anshv-dev <anshv8166@gmail.com>
2025-07-05 17:46:23 +0530

    Golang

---
+++ 2_simple_values/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//int
+	fmt.Println(1 + 1)
+	//string
+	fmt.Println("hello golang")
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+	//floats
+	fmt.Println(10.4 * 3.5)
+	fmt.Println(7.4 / 3)
+}
---
+++ 3_variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+	var name = "dfkjvn" //short-hands
+	var isAdults = true
+	var age int = 34
+
+	fmt.Println(name)
+	fmt.Println(isAdults)
+	fmt.Println(age)
+
+	//short-hand syntax
+	random := "golang"
+	fmt.Println(random)
+}
